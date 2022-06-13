48d6a4d86048a255688b547f0b15b98600d2ac44
Chohee <chohee4421@gmail.com>
2022-06-13 18:07:49 +0900

    Set up

---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from 'styled-components';
+
+const choheeTheme : DefaultTheme = {
+    backgroundColor: '#2c3e50',
+    textColor:'#27ae60',
+    accentColor:'#e74c3c',
+}
+
+export {choheeTheme}
